ba365fba3905f13f2fe00e43230089ea8cd9bc86
JTRaffield <42154699+JTRaffield@users.noreply.github.com>
2018-08-30 13:34:34 -0400

    Add files via upload

---
+++ CarLibrary/CarLibrary/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLibrary
+{
+    public class Car
+    {
+        //Properties
+        public int YearModel { get; set; }
+        public string Make { get; set; }
+        public int Speed { get; set; }
+        
+        //This is a default constructor that creates an empty car
+        public Car() { }
+
+        //Constructor (Has the same name as the class)
+        public Car(int yearModel, string make)
+        {
+            this.YearModel = yearModel;
+            this.Make = make;
+            this.Speed = 0;
+        }
+
+        //Method
+        public void Accelerate()
+        {
+            this.Speed += 5;           
+        }
+
+        //Method
+        public void Brake()
+        {
+            this.Speed -= 5;
+        }
+    }
+    public class truck
+    {
+
+    }
+}
---
+++ CarLibrary/CarType/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarLibrary;
+
+namespace CarType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(1999, "camry");
+            Console.WriteLine($"Your car is a {car.YearModel}, {car.Make}");
+
+            Console.WriteLine(car.Speed);
+
+            for (int i = 0; i < 5; i++)
+            {
+                car.Accelerate();
+                Console.WriteLine(car.Speed);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                car.Brake();
+                Console.WriteLine(car.Speed);
+            }
+            Console.ReadLine();
+
+        }
+
+        
+          
+            
+        
+    }
+}
