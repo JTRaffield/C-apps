2089094a95e2401e64dc266203ed6163ce68654c
Johnathan Raffield <42154699+JTRaffield@users.noreply.github.com>
2018-08-30 13:41:56 -0400

    Add files via upload

---
+++ 20180809 Wednesday/ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        /*static void Main(string[] args)
+        {
+            Console.WriteLine("Johnathan");
+            Console.WriteLine("300 Gonzalez Blvd, Camp Lejeune, NC, 28547");
+            Console.WriteLine("903-376-1144");
+            Console.WriteLine("Corporal");
+        }*/
+
+
+        /*static void Main()
+        {
+            Console.WriteLine("Enter the projected amount of sales.");
+            double sales = Convert.ToDouble (Console.ReadLine());
+            double P = sales * .23;
+            Console.WriteLine($"Projected amount of profit is ${P}.");
+        }*/
+
+
+        static void Main()
+        {
+            Console.WriteLine("Enter Price of first item.");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Price of second item.");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Price of third item.");
+            double c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Price of fourth item.");
+            double d = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Price of fifth item.");
+            double e = Convert.ToDouble(Console.ReadLine());
+
+            double Subtotal = a + b + c + d + e;
+            double tax = Subtotal * .06;
+            double final = Subtotal + tax;
+
+            Console.WriteLine($"Your Subtotal is ${Subtotal}.");
+            Console.WriteLine($"Your Sales tax is ${tax}.");
+            Console.WriteLine($"Your final total is ${final}.");
+
+
+            //This way the subtotal is converted to a string at the same time as it is placed in the sentence. Doing this places the currency indicator (Dollar, Pound, Euro, Yen) automatically.
+
+            //Console.WriteLine($"Subtotal of sales is {String.Format("{0:C}", Subtotal)}");
+
+        }
+
+
+    } 
+}
---
+++ 20180816/Group_20180816/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_20180816
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sales = Sales();
+            double CountyTax = sales * .02;
+            double StateTax = sales * .04;
+            double TotalTax = CountyTax + StateTax;
+            DisplayMessage(CountyTax);
+            DisplayMessage2(StateTax);
+            DisplayMessage3(TotalTax);
+            
+        }
+
+        static double Sales()
+        {
+            Console.WriteLine("Enter total sales for month");
+            double sales = Convert.ToDouble(Console.ReadLine());
+            return sales;
+        }
+
+        static double DisplayMessage(double CountyTax)
+        {
+            Console.WriteLine($"The County Tax is {CountyTax}");
+            return CountyTax;
+        }
+
+        static double DisplayMessage2(double StateTax)
+        {
+            Console.WriteLine($"The State Tax is {StateTax}");
+            return StateTax;
+        }
+
+        static double DisplayMessage3(double TotalTax)
+        {
+            Console.WriteLine($"The Total Tax is {TotalTax}");
+            Console.ReadLine();
+            return TotalTax;
+        }
+    }
+}
