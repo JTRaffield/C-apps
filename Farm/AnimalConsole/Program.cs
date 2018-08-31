1c3c285096259acf5733c966c0f4e7f4b3cf3b21
Johnathan Raffield <42154699+JTRaffield@users.noreply.github.com>
2018-08-31 08:12:44 -0400

    Add files via upload

---
+++ Farm/AnimalConsole/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnimalsClass;
+
+namespace AnimalConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets see what animals you will get in your farm!");
+
+            Animals A = new Animals
+            {
+                Animal = GetAnimalType(),
+
+                Name = GetAnimalName(),
+
+                Color = GetAnimalColor()
+            };
+
+                Console.WriteLine($"Your {A.GetColor()} {A.GetAnimal()} named {A.GetName()} says {A.GetSound()}");
+                Console.WriteLine($"{A.GetName()} eats {A.GetFood()} and gives us {A.GetProduct()}.");
+                Console.ReadLine();
+
+
+            Animals B = new Animals
+            {
+                Animal = GetAnimalType(),
+
+                Name = GetAnimalName(),
+
+                Color = GetAnimalColor()
+            };
+
+                Console.WriteLine($"Your {B.GetColor()} {B.GetAnimal()} named {B.GetName()} says {B.GetSound()}");
+                Console.WriteLine($"{B.GetName()} eats {B.GetFood()} and gives us {B.GetProduct()}.");
+                Console.ReadLine();
+
+
+            Animals C = new Animals
+            {
+                Animal = GetAnimalType(),
+
+                Name = GetAnimalName(),
+
+                Color = GetAnimalColor()
+            };
+
+                Console.WriteLine($"Your {C.GetColor()} {C.GetAnimal()} named {C.GetName()} says {C.GetSound()}");
+                Console.WriteLine($"{C.GetName()} eats {C.GetFood()} and gives us {C.GetProduct()}.");
+                Console.ReadLine();
+
+
+            Animals D = new Animals
+            {
+                Animal = GetAnimalType(),
+
+                Name = GetAnimalName(),
+
+                Color = GetAnimalColor()
+            };
+
+                Console.WriteLine($"Your {D.GetColor()} {D.GetAnimal()} named {D.GetName()} says {D.GetSound()}");
+                Console.WriteLine($"{D.GetName()} eats {D.GetFood()} and gives us {D.GetProduct()}.");
+                Console.ReadLine();
+
+            Console.WriteLine($"To recap, your farm consists of {A.GetName()} the {A.GetColor()} {A.GetAnimal()}, {B.GetName()} the {B.GetColor()} {B.GetAnimal()}, {C.GetName()} the {C.GetColor()} {C.GetAnimal()}, and {D.GetName()} the {D.GetColor()} {D.GetAnimal()}.");
+            Console.ReadLine();
+
+        }
+
+        static string GetAnimalType()
+        {
+                string input;
+            do
+            {
+                Console.WriteLine("Press 1-4 for an animal or 0 to exit.");
+                input = Console.ReadLine();
+            } while (!IsNumeric(input) || !IsInRange(Convert.ToInt32(input)));
+            if (input == "0") Environment.Exit(0);
+                return input;          
+        }
+
+        static string GetAnimalName()
+        {            
+                string name;
+            do
+            {
+                Console.WriteLine("Press 1-4 for a name or 0 to exit.");
+                name = Console.ReadLine();
+            } while (!IsNumeric(name) || !IsInRange(Convert.ToInt32(name)));
+            if (name == "0") Environment.Exit(0);
+                return name;
+        }
+
+        static string GetAnimalColor()
+        {
+            string color;
+            do
+            {
+                Console.WriteLine("Press 1-4 for a color or 0 to exit.");
+                color = Console.ReadLine();
+            } while (!IsNumeric(color) || !IsInRange(Convert.ToInt32(color)));
+            if (color == "0") Environment.Exit(0);
+            return color;
+        }
+
+        static bool IsNumeric(string input)
+        {
+            return int.TryParse(input, out int test);
+        }
+
+        static bool IsInRange(int input)
+        {
+            return (input >= 0 && input <= 4);
+        }
+    }
+}
---
+++ Farm/AnimalsClass/Animals.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalsClass
+{
+    public class Animals
+    {
+        public string Animal { get; set; } 
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string Sound { get; set; }
+        public string Food { get; set; }
+        public string Product { get; set; }
+        
+        public Animals()
+        {
+         
+        }
+
+        public string GetAnimal()
+        {
+            if (this.Animal == "1") return "Cow";
+            if (this.Animal == "2") return "Dog";
+            if (this.Animal == "3") return "Horse";
+            if (this.Animal == "4") return "Pig";
+            return Animal;
+        }
+
+        public string GetName()
+        {
+            if (this.Name == "1") return "Rex";
+            if (this.Name == "2") return "Jenney";
+            if (this.Name == "3") return "Billy";
+            if (this.Name == "4") return "Chesty";
+            return Name;
+        }
+
+        public string GetColor()
+        {
+            if (this.Color == "1") return "Black";
+            if (this.Color == "2") return "Brown";
+            if (this.Color == "3") return "Grey";
+            if (this.Color == "4") return "Orange Spotted";
+            return Color;
+        }
+
+        public string GetSound()
+        {
+            if (this.Animal == "1") return this.Sound = "Moo!";
+            if (this.Animal == "2") return this.Sound = "Woof!";
+            if (this.Animal == "3") return this.Sound = "Neigh!";
+            if (this.Animal == "4") return this.Sound = "Oink!";
+            return Sound;
+        }
+
+        public string GetFood()
+        {
+            if (this.Animal == "1") return this.Food = "grass";
+            if (this.Animal == "2") return this.Food = "dog chow";
+            if (this.Animal == "3") return this.Food = "hay";
+            if (this.Animal == "4") return this.Food = "oats";
+            return Food;
+        }
+
+        public string GetProduct()
+        {
+            if (this.Animal == "1") return this.Product = "beef";
+            if (this.Animal == "2") return this.Product = "company";
+            if (this.Animal == "3") return this.Product = "rides";
+            if (this.Animal == "4") return this.Product = "bacon";
+            return Product;
+        }
+    }
+}
