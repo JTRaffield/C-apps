944cb4eee7e4bc81c77bcf1f3904c66fd14ed37a
Johnathan Raffield <42154699+JTRaffield@users.noreply.github.com>
2018-09-05 12:46:34 -0400

    Add files via upload

---
+++ StructsAndEnums/StructsAndEnums/Date.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace StructsAndEnums
+{
+    struct Date
+    {
+        private int year;
+        private Month month;
+        private int day;
+        public Date(int ccyy, Month mm, int dd)
+        {
+            this.year = ccyy;
+            this.month = mm;
+            this.day = dd - 1;
+        }
+
+        public override string ToString()
+        {
+            string data = $"{this.month} {this.day + 1} {this.year}";
+            return data;            
+        }
+
+        public void AdvanceMonth()
+        {
+            this.month++;
+            if (this.month == Month.December + 1)
+            {
+                this.month = Month.January;
+                this.year++;
+            }
+        }
+    }
+
+
+    //class Date
+    //{
+    //    private int year;
+    //    private Month month;
+    //    private int day;
+    //    public Date(int ccyy, Month mm, int dd)
+    //    {
+    //        this.year = ccyy;
+    //        this.month = mm;
+    //        this.day = dd - 1;
+    //    }
+
+    //    public override string ToString()
+    //    {
+    //        string data = $"{this.month} {this.day + 1} {this.year}";
+    //        return data;
+    //    }
+
+    //    public void AdvanceMonth()
+    //    {
+    //        this.month++;
+    //        if (this.month == Month.December + 1)
+    //        {
+    //            this.month = Month.January;
+    //            this.year++;
+    //        }
+    //    }
+    //}
+}
